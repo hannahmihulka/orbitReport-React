035fd6610960998835a9d2cb012483c45fe9e1a7
Hannah Mihulka <hannahmihulka@gmail.com>
2023-08-07 20:05:50 -0500

    created filterByType function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import useState from 'react';
+import satData from './components/satData'; 
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  let displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const filteredSats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(filteredSats);
+  };
+
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
   );
-}
+  }
+
 
 export default App;
